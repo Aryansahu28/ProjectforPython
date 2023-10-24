cd55cd7ede02cb69cee775731dfa059196382ead
Aryan Sahu <aryansahu.vibranium@gmail.com>
2023-10-24 14:48:54 +0530

    d

--- texttospeech.py
+++ texttospeech.py
@@ -1,9 +1,8 @@
 from gtts import gTTS
 import os
-myText = ['Shoutout to Rahul','Shoutout to Ravi','Shoutout to Aaryan']
+myText = 'Shoutout to Rahul,Shoutout to Ravi ,Shoutout to Aaryan'
 
 language = "en"
-for i in range(0,3):
-    myobj = gTTS(text=myText[f"{i}"], lang=language, slow=False)
-    myobj.save(f"shoutout[{i}].mp3")
-    os.system(f"shoutout.mp3")
+myobj = gTTS(text=myText, lang=language, slow=False)
+myobj.save("shoutout.mp3")
+os.system("shoutout.mp3")
