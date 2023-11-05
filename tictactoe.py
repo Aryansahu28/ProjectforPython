4a44f509738d0987343a313b68707de2e8039008
Aryan Sahu <aryansahu.vibranium@gmail.com>
2023-11-05 19:24:24 +0530

    tic tac toe game

---
+++ tictactoe.py
@@ -0,0 +1,25 @@
+import random
+print("This is tic tac toe game , I'm assuming you know the rules already")
+print("For the first person his sign is ❎ and for the second person the sign is ⭕")
+
+player_1 = input("Enter the name of player 1: ")
+player_2 = input("Enter the name of player 2: ")
+x = random.randint(0,1)
+
+if x == 1:
+    print(f"{player_1} is first one to go")
+
+else:
+    print(f"{player_2} is first to go")
+
+
+
+print(" 1|2|3\n-------\n 4|5|6\n-------\n 7|8|9  ")
+
+if chance
+
+
+
+
+
+    
